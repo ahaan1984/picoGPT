ba6d9f2cd65d5bc5e339caebe2d114b3f48de657
ahaan <ahaansekhar1984@gmail.com>
2023-10-09 23:07:41 +0530

    create bigram language model

---
+++ bigram.py
@@ -0,0 +1,46 @@
+import typing
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+batch_size:int = 8 # how many independent sequences will we process in parallel?
+block_size:int = 16 # what is the maximum context length for predictions?
+max_iters:int = 4000
+eval_interval:int = 100
+learning_rate:int = 1e-3
+device:str = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters:int = 100
+n_embd:int = 32
+n_head:int = 2
+n_layer:int = 2
+dropout:int = 0.0
+
+
+class Bigram(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init()
+        self.token_embd = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None) -> torch.Tensor:
+        logits = self.token_embd(idx)
+        if targets is None:
+            loss = None
+        else:
+            b, t, c = logits.shape
+            logits = logits.view(b*t, c)
+            targets = targets.view(b*t)
+            criterion = nn.CrossEntropyLoss()
+            loss = criterion(logits, targets)
+
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens) -> torch.Tensor:
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx_next = idx_next.to(device)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
